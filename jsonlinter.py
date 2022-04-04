c00e098613861bf314205711288709d3b0ddea50
William Rehwinkel <willrehwinkel@gmail.com>
2022-04-03 21:42:51 -0400

    json linter python script

---
+++ jsonlinter.py
@@ -0,0 +1,44 @@
+# python3 lintjson.py
+# William Rehwinkel / obiesource, 2022
+
+import sys
+import os
+import json
+import traceback
+
+def getJsonFiles():
+    # list all files recursively borrowed from https://pythonexamples.org/python-get-list-of-all-files-in-directory-and-sub-directories/
+    filelist = list()
+    path = "./"
+    
+    for root, dirs, files in os.walk(path):
+	    for file in files:
+            #append the file name to the list
+		    filelist.append(os.path.join(root,file))
+    
+    #print all the file names
+    jsonFiles = list()
+    
+    for name in filelist:
+        if name.lower().endswith(".json"):
+            jsonFiles.append(name)
+    return jsonFiles
+
+def lintJson(filename):
+    with open(filename) as file:
+        try: json.load(file)
+        except Exception as e:
+            print("Error while parsing " + file)
+            traceback.print_tb(e)
+    return False
+
+if __name__ == "__main__":
+    jsonFiles = getJsonFiles()
+    failed = False
+    for file in jsonFiles:
+        if lintJson(file) == True:
+            failed = True
+
+    if failed:
+        sys.exit(1)
+
