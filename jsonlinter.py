56c9fab26d71e586c8aa65b788955159954d32a4
William Rehwinkel <willrehwinkel@gmail.com>
2022-04-03 22:14:41 -0400

    check for required keys in members information json files

--- jsonlinter.py
+++ jsonlinter.py
@@ -24,14 +24,39 @@ def getJsonFiles():
             jsonFiles.append(name)
     return jsonFiles
 
+REQUIRED_KEYS = (
+        "name",
+        "pronouns",
+        "class",
+        "socials",
+        "bio",
+        "projects"
+        )
+
 def lintJson(filename):
     print("Checking file " + filename)
     with open(filename) as file:
-        try: json.load(file)
+        try: js = json.load(file)
         except Exception as e:
             print("Error while parsing " + filename)
             traceback.print_tb(e)
-    return False
+            return True
+
+        failed = False
+        if filename.startswith("./members/"):
+            # check for required keys
+            for k in REQUIRED_KEYS:
+                if k not in js:
+                    print("Key " + k + " required but not found in members JSON file.")
+                    failed = True
+            # check for any keys in members 
+            for k in js:
+                if k not in REQUIRED_KEYS:
+                    print("Key " + k + " in members JSON file is not required by website.")
+                    failed = True
+                
+
+    return failed
 
 if __name__ == "__main__":
     jsonFiles = getJsonFiles()
