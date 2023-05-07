f81fbb7d8618313e0f5df6de6ac53b213debd7fa
Ajai Nelson <22969541+Aurelius333@users.noreply.github.com>
2023-05-07 01:09:24 -0400

    Use react router best practices and fix scroll position issue on navigation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React, { StrictMode } from "react";
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Outlet,
+  ScrollRestoration,
+} from "react-router-dom";
 import "./App.css";
 
 import { Navbar } from "./components";
 import { Home, About, Contact, Members, Member, Resources } from "./pages";
 
+const PageLayout = () => {
+  return (
+    <div>
+      <Navbar />
+      <Outlet />
+      <ScrollRestoration />
+    </div>
+  );
+};
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route element={<PageLayout />}>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/members" element={<Members />} />
+      <Route path="/members/:name" element={<Member />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/resources" element={<Resources />} />
+    </Route>
+  )
+);
+
 const App = () => {
   return (
-    <Router>
-      <div className="App">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/members" element={<Members />} />
-          <Route path="/members/:name" element={<Member />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/resources" element={<Resources />} />
-        </Routes>
-      </div>
-    </Router>
+    <StrictMode>
+      <RouterProvider router={router} />
+    </StrictMode>
   );
 };
 
