410f5d36db1c0b4d728f25e853c3f233bf489f27
Yen Mai <110384864+yen-mai-codes@users.noreply.github.com>
2023-09-13 23:49:17 -0400

    bug: now shows all projects

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -12,17 +12,29 @@ const Projects = () => {
       const data = await (
         await fetch("https://api.github.com/orgs/ObieSource/repos")
       ).json();
+
+      const dataFiltered = []
+
+      for (let i = 0; i < data.length; i++) {
+        if (data[i].full_name != "ObieSource/.github") {
+          dataFiltered.push(data[i])
+        }
+      }
       
-      const dataSorted = data.sort((a, b) => b.updated_at.localeCompare(a.updated_at))
+
+      const dataSorted = dataFiltered.sort((a, b) => b.updated_at.localeCompare(a.updated_at))
       const dataSortedTuples = []
       var curTuple = []
 
-      for (var i in dataSorted) {
+      for (let i = 0; i < dataSorted.length; i++) {
         if (curTuple.length == 3) {
           dataSortedTuples.push(curTuple)
           curTuple = []
         }
         curTuple.push(dataSorted[i])
+        if (i == dataSorted.length - 1) { 
+          dataSortedTuples.push(curTuple)
+        }
       }
 
       // set state when the data received
@@ -48,13 +60,12 @@ const Projects = () => {
         &#123;See Our Projects&#125;
         </div>
 
-      {/* <div className="obs__projects-container"> */}
       <div className="obs__projects-carousel__container">
 
         <Carousel interval={null} controls={false} activeIndex={index} onSelect={handleSelect} className="obs__projects-carousel">
           {projects.map((projectTuples, index) => {
             return (
-              <Carousel.Item> 
+              <Carousel.Item key={index}> 
                 <div className="obs__projects-container">
                 {projectTuples.map((project, index) => {
                   return (
@@ -72,9 +83,9 @@ const Projects = () => {
                           </div>
                         </div>
                         <div className="obs__projects-project-heading-description">
-                          {project.description.indexOf(' Project') != -1 ? project.description.substring(0, project.description.indexOf(' Project')) : project.description}
+                          {project.description != null ? project.description.indexOf(' Project') != -1 ? project.description.substring(0, project.description.indexOf(' Project')) : project.description : ""}
                           <div className="obs__projects-project-heading-description-leader">
-                          {project.description.indexOf(' Project') == -1 ? "" : project.description.substring(project.description.indexOf(' Project') + 9)}
+                          {project.description != null ? project.description.indexOf(' Project') == -1 ? "" : project.description.substring(project.description.indexOf(' Project') + 9) : ""}
                           </div>
                         </div>
                     </div>
