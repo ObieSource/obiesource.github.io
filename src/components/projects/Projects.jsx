beb2aaaf9c6b45b270d1469f589eedc5aeb77541
Ajai Nelson <22969541+AjaiKN@users.noreply.github.com>
2023-09-14 19:45:53 -0400

    Remove bootstrap

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,7 +1,5 @@
 import React, { useEffect, useState } from "react";
 import "./projects.css";
-import Carousel from 'react-bootstrap/Carousel';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Projects = () => {
   const [projects, setProjects] = useState(null);
@@ -13,16 +11,17 @@ const Projects = () => {
         await fetch("https://api.github.com/orgs/ObieSource/repos")
       ).json();
 
-      const dataFiltered = []
+      const dataFiltered = [];
 
       for (let i = 0; i < data.length; i++) {
         if (data[i].full_name != "ObieSource/.github") {
-          dataFiltered.push(data[i])
+          dataFiltered.push(data[i]);
         }
       }
-      
 
-      const dataSorted = dataFiltered.sort((a, b) => b.updated_at.localeCompare(a.updated_at))
+      const dataSorted = dataFiltered.sort((a, b) =>
+        b.updated_at.localeCompare(a.updated_at),
+      );
       // const dataSortedTuples = []
       // var curTuple = []
 
@@ -32,15 +31,13 @@ const Projects = () => {
       //     curTuple = []
       //   }
       //   curTuple.push(dataSorted[i])
-      //   if (i == dataSorted.length - 1) { 
+      //   if (i == dataSorted.length - 1) {
       //     dataSortedTuples.push(curTuple)
       //   }
       // }
 
       // set state when the data received
-      setProjects(
-        dataSorted
-      );
+      setProjects(dataSorted);
     };
 
     dataFetch();
@@ -56,37 +53,48 @@ const Projects = () => {
 
   return (
     <div className="obs__projects">
-        <div className="obs__projects-header">
-        &#123;See Our Projects&#125;
-        </div>
+      <div className="obs__projects-header">&#123;See Our Projects&#125;</div>
 
       <div className="obs__projects-flex__container">
-          {projects.map((project, index) => {
-            return (
-              <div
-                className="obs__projects-project animate flip"
-                key={project.name}
-                style={{ animationDelay: 0.5 + index * 0.25 + "s" }}
-              >
-                <div className="obs__projects-project-heading">
-                  <div className="obs__projects-project-heading-title">
-                    <a href={project.html_url}>{project.name}</a>
-                  </div>
-                  <div className="obs__projects-project-heading-status">
-                    {project.license?.spdx_id}
-                  </div>
+        {projects.map((project, index) => {
+          return (
+            <div
+              className="obs__projects-project animate flip"
+              key={project.name}
+              style={{ animationDelay: 0.5 + index * 0.25 + "s" }}
+            >
+              <div className="obs__projects-project-heading">
+                <div className="obs__projects-project-heading-title">
+                  <a href={project.html_url}>{project.name}</a>
                 </div>
-                <div className="obs__projects-project-heading-description">
-                  {project.description != null ? project.description.indexOf(' Project') != -1 ? project.description.substring(0, project.description.indexOf(' Project')) : project.description : ""}
-                  <div className="obs__projects-project-heading-description-leader">
-                  {project.description != null ? project.description.indexOf(' Project') == -1 ? "" : project.description.substring(project.description.indexOf(' Project') + 9) : ""}
-                  </div>
+                <div className="obs__projects-project-heading-status">
+                  {project.license?.spdx_id}
                 </div>
+              </div>
+              <div className="obs__projects-project-heading-description">
+                {project.description != null
+                  ? project.description.indexOf(" Project") != -1
+                    ? project.description.substring(
+                        0,
+                        project.description.indexOf(" Project"),
+                      )
+                    : project.description
+                  : ""}
+                <div className="obs__projects-project-heading-description-leader">
+                  {project.description != null
+                    ? project.description.indexOf(" Project") == -1
+                      ? ""
+                      : project.description.substring(
+                          project.description.indexOf(" Project") + 9,
+                        )
+                    : ""}
+                </div>
+              </div>
             </div>
-          )
-                })}
-                </div>
+          );
+        })}
       </div>
+    </div>
   );
 };
 
